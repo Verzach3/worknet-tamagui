aa1447c5d21323d10e30305dd642fc9213cd4edc
Willian David <williamdavidcv@gmail.com>
2024-06-14 20:26:53 -0500

    feat: Job y Jobtype, funcionallidad

--- src/pages/start-questions/Index.tsx
+++ src/pages/start-questions/Index.tsx
@@ -1,6 +1,8 @@
 import { IonPage, IonRouterOutlet, IonText } from "@ionic/react";
 import Country from "./Country";
 import { Redirect, Route } from "react-router";
+import Job from "./job/job";
+import Jobtype from "./typejob/jobtype";
 
 function StartQuestionsIndex() {
 	return (
@@ -8,8 +10,11 @@ function StartQuestionsIndex() {
 			<Route exact path="/start-questions/country">
 				<Country />
 			</Route>
-			<Route path="/start-questions">
-				<Redirect to="/start-questions/country" />
+			<Route exact path="/start-questions/job">
+				<Job />
+			</Route>
+			<Route exact path="/start-questions/typejob">
+				<Jobtype />
 			</Route>
 		</>
 	);
